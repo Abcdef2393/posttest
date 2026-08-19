b6798bf731d95de8d4462a8d3f621ed24cd39c3a
Abcdef2393 <zac.noche@gmail.com>
2026-08-18 22:56:18 -0400

    Update webhook logging messages in server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.json());
 
 app.post("/api/webhooks/:id/:token", (req, res) => {
-    console.log("=== WEBHOOK RECEIVED ===");
+    console.log("Webhook received!");
     console.log("ID:", req.params.id);
     console.log("Token:", req.params.token);
-    console.log("Body:", req.body);
+    console.log("Content:", req.body);
 
     res.status(204).send();
 });
