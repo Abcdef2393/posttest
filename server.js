04af728cdb9daaec03a2a1792d6847dde7045750
Abcdef2393 <zac.noche@gmail.com>
2026-08-18 22:53:45 -0400

    Refactor webhook handling with dynamic parameters
    Updated webhook endpoint to use dynamic parameters for ID and token.

--- server.js
+++ server.js
@@ -3,36 +3,14 @@ const app = express();
 
 app.use(express.json());
 
-const webhook = {
-    application_id: null,
-    avatar: null,
-    channel_id: "1530720092055404776",
-    guild_id: "1530720083909808158",
-    id: "1539035219439517717",
-    name: "Captain Hook",
-    type: 1,
-    token: "iu0hIuF4r9LPYCXdAWyOUBYaruTtiFQYBA-I-0wwDWnZPCtGRKAT0_ZH-WlB-IDVbBdu",
-    url: "https://posttest-1ehw.onrender.com/api/webhook/1539035219439517717/iu0hIuF4r9LPYCXdAWyOUBYaruTtiFQYBA-I-0wwDWnZPCtGRKAT0_ZH-WlB-IDVbBdu"
-};
+app.post("/api/webhooks/:id/:token", (req, res) => {
+    console.log("=== WEBHOOK RECEIVED ===");
+    console.log("ID:", req.params.id);
+    console.log("Token:", req.params.token);
+    console.log("Body:", req.body);
 
-// GET = give information about the webhook
-app.get(
-    "/api/webhook/1539035219439517717/iu0hIuF4r9LPYCXdAWyOUBYaruTtiFQYBA-I-0wwDWnZPCtGRKAT0_ZH-WlB-IDVbBdu",
-    (req, res) => {
-        res.json(webhook);
-    }
-);
-
-// POST = receive webhook content
-app.post(
-    "/api/webhook/1539035219439517717/iu0hIuF4r9LPYCXdAWyOUBYaruTtiFQYBA-I-0wwDWnZPCtGRKAT0_ZH-WlB-IDVbBdu",
-    (req, res) => {
-        console.log("Webhook received!");
-        console.log(req.body);
-
-        res.status(204).send();
-    }
-);
+    res.status(204).send();
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running");
