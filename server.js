fb84d830c8640aeb7a39bc7a7935422c91874872
Abcdef2393 <zac.noche@gmail.com>
2026-08-18 22:44:00 -0400

    Refactor webhook endpoint to log headers and body
    Updated the webhook endpoint to log headers and respond with 'OK'.

--- server.js
+++ server.js
@@ -3,21 +3,15 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/path", (req, res) => {
-    const input = req.query.input;
-    res.send("put what your sending");
-});
-
 app.post("/api/webhook/583104927615/8fK3mQ7xV2pL9zR4", (req, res) => {
+    console.log("Webhook received!");
+    console.log("Headers:", req.headers);
+    console.log("Body:", req.body);
     const content = req.body.content;
-    const username = req.body.username;
 
-    console.log("Username:", username);
     console.log("Content:", content);
 
-    res.json({
-        success: true
-    });
+    res.status(200).send("OK");
 });
 
 app.listen(process.env.PORT || 3000, () => {
