69ccf9a91f276b1d53ffb43e9847530fd2bc33ee
Abcdef2393 <zac.noche@gmail.com>
2026-08-18 22:39:48 -0400

    Refactor server.js to clean up code structure

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 
-// extra
-// app.use(express.placeholder());
-
-// replace the path with anything, remove the path, because it can affect the link, make sure it is like: ("", (req, res) => { to remove 
-// the path
-
-app.get("path", (req, res) => {
-
-    // use this as the input (what is being sent)
-
+app.get("/path", (req, res) => {
     const input = req.query.input;
-    res.send("put what your sending")
+    res.send("put what your sending");
 });
 
 app.post("/api/webhook/583104927615/8fK3mQ7xV2pL9zR4", (req, res) => {
-	console.log(req.body);
-	res.send("Recieved!");
+    console.log(req.body);
+    res.send("Received!");
 });
 
 app.listen(process.env.PORT || 3000, () => {
